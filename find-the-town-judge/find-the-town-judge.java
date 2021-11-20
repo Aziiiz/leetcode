65421105ca4b7d7094219a8662e95f1816516ea5
Abdulaziz Sirojiddinov <asirojiddinov96@gmail.com>
2021-11-20 18:37:40 +0900

    Time: 3 ms (81.82%), Space: 46.6 MB (74.76%) - LeetHub

--- find-the-town-judge/find-the-town-judge.java
+++ find-the-town-judge/find-the-town-judge.java
@@ -2,22 +2,14 @@ class Solution {
     public int findJudge(int n, int[][] trust) {
         if(trust.length == 0) return n ==1 ? n : -1;
         int[] counter = new int[n+1];
-        int result=0;
-        int resultValue=0;
-        Set<Integer> set = new HashSet<Integer>();
         for(int i=0; i<trust.length; i++) {
-             set.add(trust[i][0]);
-             counter[trust[i][1]] +=1;
-            
-            if(resultValue < counter[trust[i][1]]) {
-                resultValue = counter[trust[i][1]];
-                result = trust[i][1];
-            }
+                counter[trust[i][0]]--;
+                counter[trust[i][1]]++;
         }
         
-        if(n == resultValue+1 && !set.contains(result)) return result;
+        for(int i=0; i<counter.length; i++) {
+            if(n == counter[i]+1) return i;
+        }
         return -1;
-        
-        
     }
 }
